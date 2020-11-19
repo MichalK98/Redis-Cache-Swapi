5a01945dd0493f46146599f1413eca70648a50d7
Michal K <michal10203040@gmail.com>
2020-11-19 14:34:40 +0100

    Get :id from /people/:id

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
+app.get("/people/:id", (req, res) => {
+  console.log(req.params.id);
+});
+
 app.listen(port, () => {
   console.log(`Listen on http://localhost:${port}`);
-})
+});
