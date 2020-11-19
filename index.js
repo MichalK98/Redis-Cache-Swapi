a97cae007ac8117ca06e2b22355fbec906051a63
Michal K <michal10203040@gmail.com>
2020-11-19 15:04:58 +0100

    Fetch data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fetch = require("node-fetch");
+
 const app = express();
 
 const port = 8080;
@@ -7,9 +9,23 @@ app.get("/", (req, res) => {
   res.sendStatus(200);
 });
 
-app.get("/people/:id", (req, res) => {
-  console.log(req.params.id);
-});
+// Make request to swapi
+async function getPeople(req, res, next) {
+  try {
+    console.log("Fetching data...");
+
+    const people = req.params.id;
+    const response = await fetch(`https://swapi.dev/api/people/${people}`);
+    const data = await response.json();
+    
+    res.send(data.name)
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
+}
+
+app.get("/people/:id", getPeople);
 
 app.listen(port, () => {
   console.log(`Listen on http://localhost:${port}`);
